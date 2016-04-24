36e734f4b5907e760de0cc3cc0e4febf648156a6
William Chavez-Salinas <wchavezsalinas@gmail.com>
2016-04-24 19:08:49 -0400

    more design implementations

---
+++ app/about/about-controller.js
@@ -0,0 +1,9 @@
+(function() {
+    angular.module('vvilliann')
+      .controller('AboutController', ['$scope', '$http', '$interval',
+                             function($scope,   $http,   $interval) {
+
+
+
+      }]);
+}());
---
+++ app/navigation/navigation-controller.js
@@ -0,0 +1,9 @@
+(function() {
+    angular.module('vvilliann')
+      .controller('NavigationController', ['$scope', '$http', '$interval',
+                             function($scope,   $http,   $interval) {
+
+
+
+      }]);
+}());
